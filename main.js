ba2a48b8a1042db82adb2e8eb2eccb06232ef28d
Agustin Ronchi <agustinronchi15@gmail.com>
2023-09-21 06:38:21 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const hamburgerMenu = document.querySelector('#hamburger-menu');
+const navbar = document.querySelector('#navbar');
+const closeBtn = document.querySelector('#close-btn');
+const featuresBtn = document.querySelector('#features-btn');
+const featureUl = document.querySelector('#features-ul');
+const companyBtn = document.querySelector('#company-btn');
+const companyUl = document.querySelector('#company-ul');
+
+const featuresBtnArrow = document.querySelector('#features-btn-arrow');
+const companyBtnArrow = document.querySelector('#company-btn-arrow');
+
+hamburgerMenu.addEventListener('click', () => {
+    navbar.style.display = 'block';    
+});
+
+closeBtn.addEventListener('click', () => {
+
+    const screenSize = window.innerWidth;
+
+    if (screenSize >= 768){
+        navbar.style.display = 'block';
+    }else{
+        navbar.style.display = 'none';
+    }
+});
+
+featuresBtn.addEventListener('click', () => {
+    featureUl.style.display = (featureUl.style.display === 'block') ? 'none' : 'block'
+
+    featuresBtnArrow.style.transition = 'transform 0.3s ease'
+    featuresBtnArrow.style.transform = (featuresBtnArrow.style.transform === 'rotate(180deg)') ? 'rotate(0deg)' : 'rotate(180deg)'
+});
+
+companyBtn.addEventListener('click', () => {
+    companyUl.style.display = (companyUl.style.display === 'block') ? 'none' : 'block'
+
+    companyBtnArrow.style.transition = 'transform 0.3s ease'
+    companyBtnArrow.style.transform = (companyBtnArrow.style.transform === 'rotate(180deg)') ? 'rotate(0deg)' : 'rotate(180deg)'
+})
